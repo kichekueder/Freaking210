eeba7ccf1b465aa55f32a13b3817c577510284b3
munhoven <munhoven@gmail.com>
2017-05-13 20:42:18 +0200

    Text fields collected to console once button pressed §2.24

--- Freaking210/ViewController.swift
+++ Freaking210/ViewController.swift
@@ -12,16 +12,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var theLabel: UILabel!
     
-    var tapCount = 0
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        tapCount += 1
-        
-        if tapCount >= 10 {
-            theLabel.text = "You tapped at least 10 times."
-            
-        }
+        print(text1.text!)
+        print(text2.text!)
         
     }
     
