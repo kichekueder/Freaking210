3b4978764ffc0a7719bc6c3eadf51fc8b8a411f1
munhoven <munhoven@gmail.com>
2017-05-13 20:48:19 +0200

    Label shows result of addition. §2.25

--- Freaking210/ViewController.swift
+++ Freaking210/ViewController.swift
@@ -18,8 +18,7 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        print(text1.text!)
-        print(text2.text!)
+        theLabel.text = "Amswer: \(Double(text1.text!)!+Double(text2.text!)!)"
         
     }
     
